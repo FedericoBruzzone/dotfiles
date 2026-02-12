56e3cf448fd68bbb3fb7226814723866955b441b
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2026-02-12 14:47:22 +0100

    Update nvim and llvm-helper bin
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- .config/nvim/lua/plugins/coqtail.lua
+++ .config/nvim/lua/plugins/coqtail.lua
@@ -1,33 +1,40 @@
 return {
     {
-        "tomtomjhj/vsrocq.nvim",
-        ft = "coq", -- Load only on Coq files
-        config = function()
-            require("vsrocq").setup({
-                lsp = {
-                    -- Standard lspconfig options
-                    on_attach = function(client, bufnr)
-                        -- Keybindings for proof navigation
-                        local opts = { buffer = bufnr, silent = true }
-                        vim.keymap.set("n", "<C-l>", "<cmd>VsRocq stepForward<CR>", opts)
-                        vim.keymap.set("n", "<C-h>", "<cmd>VsRocq stepBackward<CR>", opts)
-                        vim.keymap.set("i", "<C-l>", "<cmd>VsRocq stepForward<CR>", opts)
-                        vim.keymap.set("i", "<C-h>", "<cmd>VsRocq stepBackward<CR>", opts)
-                        vim.keymap.set("n", "<leader>ch", "<cmd>VsRocq interpretToPoint<CR>", opts)
-                        -- vim.keymap.set("n", "<leader>ce", "<cmd>VsRocq interpretToEnd<CR>", opts)
-                    end,
-                },
-                vsrocq = {
-                    proof = {
-                        mode = "Manual", -- "Continuous" or "Manual"
-                        cursor = { sticky = true },
-                    },
-                },
-            })
+        'whonore/Coqtail',
+        init = function()
+            vim.g.loaded_coqtail = 1
+            vim.g["coqtail#supported"] = 0
         end,
     },
     {
-        "whonore/Coqtail",
-        ft = "coq",
+        "tomtomjhj/vsrocq.nvim",
+        ft = "coq", -- Load only on Coq files
+        dependecies = {
+            'whonore/Coqtail',
+        },
+        opts = {
+            lsp = {
+                -- Standard lspconfig options
+                on_attach = function(client, bufnr)
+                    -- Keybindings for proof navigation
+                    local opts = { buffer = bufnr, silent = true }
+                    vim.keymap.set("n", "<C-l>", "<cmd>VsRocq stepForward<CR>", opts)
+                    vim.keymap.set("n", "<C-h>", "<cmd>VsRocq stepBackward<CR>", opts)
+                    vim.keymap.set("i", "<C-l>", "<cmd>VsRocq stepForward<CR>", opts)
+                    vim.keymap.set("i", "<C-h>", "<cmd>VsRocq stepBackward<CR>", opts)
+                    vim.keymap.set("n", "<leader>ch", "<cmd>VsRocq interpretToPoint<CR>", opts)
+                    -- vim.keymap.set("n", "<leader>ce", "<cmd>VsRocq interpretToEnd<CR>", opts)
+                end,
+            },
+            vsrocq = {
+                proof = {
+                    -- mode = "Manual", -- "Continuous" or "Manual"
+                    -- cursor = { sticky = true },
+                },
+                completion = {
+                    enable = true,
+                },
+            },
+        }
     },
 }
