af4c0d97edc35c02408d90824f6e121ec68159c9
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2026-06-18 09:29:25 +0200

    Up nvim
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

---
+++ .config/nvim/lua/UNUSED/supermaven.lua
@@ -0,0 +1,11 @@
+return {
+    -- "supermaven-inc/supermaven-nvim",
+    -- keymaps = {
+    --     accept_suggestion = "<Tab>",
+    --     clear_suggestion = "<C-]>",
+    --     accept_word = "<C-j>",
+    -- },
+    -- config = function()
+    --     require("supermaven-nvim").setup({})
+    -- end
+}
